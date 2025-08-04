4d33d8f0cb1478679f4f0c4a2aed7d09c2d2f288
Chrisn N <hi.chrisn@gmail.com>
2025-08-03 22:45:42 -0300

    added config

---
+++ scraping/config.py
@@ -0,0 +1,16 @@
+# wikipedia filmography pages
+scream_queens_urls = {
+    "Jamie Lee Curtis": "https://en.wikipedia.org/wiki/List_of_Jamie_Lee_Curtis_performances",
+    "Neve Campbell": "https://en.wikipedia.org/wiki/Neve_Campbell",
+    "Florence Pugh": "https://en.wikipedia.org/wiki/Florence_Pugh",
+    "Vera Farmiga": "https://en.wikipedia.org/wiki/Vera_Farmiga_on_screen_and_stage",
+    "Megumi Okina": "https://en.wikipedia.org/wiki/Megumi_Okina",
+    "Anya Taylor-Joy": "https://en.wikipedia.org/wiki/Anya_Taylor-Joy",
+}
+
+WAIT_TIME_SHORT = (5, 10)
+WAIT_TIME_LONG = (10, 15)
+WIKI_BASE_URL = 'https://en.wikipedia.org'
+
+HORROR_KEYWORDS = ['horror', 'terror', 'slasher',
+                   'thriller', 'supernatural', 'suspense']
--- scraping/main.py
+++ scraping/main.py
@@ -5,20 +5,6 @@ import time
 import requests
 from bs4 import BeautifulSoup
 
-# wikipedia filmography pages
-scream_queens_urls = {
-    "Jamie Lee Curtis": "https://en.wikipedia.org/wiki/List_of_Jamie_Lee_Curtis_performances",
-    "Neve Campbell": "https://en.wikipedia.org/wiki/Neve_Campbell",
-    "Florence Pugh": "https://en.wikipedia.org/wiki/Florence_Pugh",
-    "Vera Farmiga": "https://en.wikipedia.org/wiki/Vera_Farmiga_on_screen_and_stage",
-    "Megumi Okina": "https://en.wikipedia.org/wiki/Megumi_Okina",
-    "Anya Taylor-Joy": "https://en.wikipedia.org/wiki/Anya_Taylor-Joy",
-}
-
-WAIT_TIME_SHORT = (5, 10)
-WAIT_TIME_LONG = (10, 15)
-WIKI_BASE_URL = 'https://en.wikipedia.org'
-
 
 def getPage(url):
     try:
@@ -35,8 +21,6 @@ def is_horror_related(url):
     if not bs:
         return False
 
-    keywords = ['horror', 'terror', 'slasher',
-                'thriller', 'supernatural', 'suspense']
     text = bs.get_text().lower()
 
     return any(word in text for word in keywords)
