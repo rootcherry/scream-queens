15e2ec853a7b3178a3a04cc5e7a310df6f07e231
Chris N <hi.chrisn@gmail.com>
2026-01-09 01:17:41 -0300

    feat(filters): add filter by survival count

---
+++ dsa/filters/filtersBySurvival.js
@@ -0,0 +1,11 @@
+const filteredBySurvival = (index) => {
+  return new Map(
+    [...index].filter(
+      ([_, profile]) =>
+        profile.stats.survived_count !== null &&
+        profile.stats.survived_count > 0
+    )
+  );
+};
+
+export default filterBySurvival;
