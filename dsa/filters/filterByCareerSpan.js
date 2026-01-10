1bcd3ffffe70093421896b934dcb7c7a88a10b0c
Chris N <hi.chrisn@gmail.com>
2026-01-10 00:02:27 -0300

    feat(dsa): add filter by career span

---
+++ dsa/filters/filterByCareerSpan.js
@@ -0,0 +1,11 @@
+const filterByCareerSpan = (index, minYears) => {
+  return new Map(
+    [...index].filter(([_, profile]) => {
+      const span = profile.stats.career_span;
+      if (!span || span.length !== 2) return false;
+
+      const [startYear, endYear] = span;
+      return endYear - startYear >= minYears;
+    })
+  );
+};
