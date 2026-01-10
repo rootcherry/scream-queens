d0a8161c82656a81ea9c1c30f6372ee6e7da8ad3
Chris N <hi.chrisn@gmail.com>
2026-01-09 23:27:32 -0300

    feat(dsa): implement filterByFilmCount using Map

---
+++ dsa/filters/filterByFilmCount.js
@@ -0,0 +1,9 @@
+const filterByFilmCount = (index, minFilms) => {
+  const entries = [...index];
+
+  const filteredEntries = entries.filter(
+    ([_, profile]) => profile.stats.horror_count >= minFilms
+  );
+
+  return new Map(filteredEntries);
+};
